1664753c7598255193f6ab20af0338787e58c782
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-02-08 23:44:28 +0200

    username and password config , also showing the session id

--- src/main/java/com/denzel/SpringSecurity/HelloController.java
+++ src/main/java/com/denzel/SpringSecurity/HelloController.java
@@ -3,11 +3,13 @@ package com.denzel.SpringSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
     
     @GetMapping("/")
-    public String greet(){
-        return "Hellow DenzelSE";
+    public String greet(HttpServletRequest request){
+        return "Hellow DenzelSE " + request.getSession().getId();
     }
 }
