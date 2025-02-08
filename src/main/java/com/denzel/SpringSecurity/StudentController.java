bc57844b459fce6ccd718c1e4b86a0b686624add
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-02-09 00:33:13 +0200

    Cross Site Request Forgery Token (CSRF Token)

--- src/main/java/com/denzel/SpringSecurity/StudentController.java
+++ src/main/java/com/denzel/SpringSecurity/StudentController.java
@@ -1,10 +1,16 @@
 package com.denzel.SpringSecurity;
 
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class StudentController {
@@ -17,5 +23,16 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken)request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
     }
 }
